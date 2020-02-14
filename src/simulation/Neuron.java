3ec7eb11cfceb5a8b550a43ab63d4441c739e912
justin <justin_kasowski@ucsb.edu>
2020-02-13 17:36:31 -0800

    Initial Commit

--- src/simulation/Neuron.java
+++ src/simulation/Neuron.java
@@ -1,4 +1,34 @@
 package simulation;
 
+import simulation.general.Location;
+
 public class Neuron {
+
+    /**
+     * Declares the possible pre-built neurons
+     */
+    public enum PrebuiltNeuron{
+        //TODO find different neuron types and list their names here
+        RS_EXC, RS_INH, FS_EXC, FS_INH;
+    }
+
+    /**
+     * Creates an instance of the neuron with the following prebuilt settings
+     * @param pbn
+     */
+    public Neuron(PrebuiltNeuron pbn, Location loc){
+        //TODO find the parameters for the prebuilt neurons specified in PrebuiltNeuron
+       switch (pbn)
+           case CLASS1
+
+    }
+
+    /**
+     * Typical creation of a Neuron
+     * @param
+     */
+     public Neuron(int a, int b, int c, int d, Location loc){
+
+     }
+
 }
---
+++ src/simulation/general/Location.java
@@ -0,0 +1,16 @@
+package simulation.general;
+
+/**
+ * Tuple usable as neuron location
+ */
+public class Location {
+    public int x = 0;
+    public int y = 0;
+    public int z = 0;
+
+    public Location(int a, int b, int c){
+        x = a;
+        y = b;
+        z = c;
+    }
+}
