e34a84e2011943c8033446ad15f000aed464ab0b
User <sb.jsmith123@gmail.com>
2020-02-14 23:07:47 -0800

    Initial commits

--- src/simulation/Main.java
+++ src/simulation/Main.java
@@ -1,13 +1,18 @@
 package simulation;
 
+import org.jfree.chart.ChartFactory;
+
 public class Main {
     public static void main(String[] args) {
         int timeSteps = 1000;
         Neuron singleNeuron = new Neuron(Neuron.PrebuiltNeuron.RS, new Location(0, 0, 0));
 
+        double[] singleNeuronVoltages = new double[timeSteps];
+
         for (int t = 0; t < timeSteps; t++) {
             singleNeuron.changeVoltage(5);
             singleNeuron.update();
+            singleNeuronVoltages[t] = singleNeuron.getVoltage();
         }
     }
 }
